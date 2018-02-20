0f4fcf9b878488826d56723f0b355f3ef19de7da
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-02-20 08:28:16 +0100

    webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const initialState = 0;
+
+export const counter = (state = initialState, action) => {
+	return state;
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,10 @@
+import {counter} from './index.js';
 
 describe("Test jest", () => {
 	it("Should sum two numbers", () => {
 		expect(1+2).toBe(3);	
 	});
+	it("Schould return default state on no actinos to the counter", () => {
+		expect(counter()).toBe(0);
+	})
 })
