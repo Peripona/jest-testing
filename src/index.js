9e5f02df00fe32f2e7ee715c1d114332e4e52365
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-02-20 08:42:31 +0100

    build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const initialState = 0;
 
+import _ from 'lodash';
+function component() {
+var element = document.createElement('div');
+
+element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+return element;
+}
+
+document.body.appendChild(component());
+
 export const counter = (state = initialState, action) => {
 	return state;
 }
