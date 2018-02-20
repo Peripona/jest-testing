a011f4bc9046f07a4b5275409cd2b90dd351df38
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-02-20 08:52:46 +0100

    css and images webpack config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-const initialState = 0;
 
 import _ from 'lodash';
+import './index.css';
+
 function component() {
 var element = document.createElement('div');
 
 element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+element.classList.add('hello');
 
 return element;
 }
 
 document.body.appendChild(component());
+
+const initialState = 0;
 
 export const counter = (state = initialState, action) => {
 	return state;
