4095867c32b0d34cad8b2690e248ba4dd29ab71b
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-02-20 09:47:02 +0100

    added fonts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 import _ from 'lodash';
 import './index.css';
+import Susj from './assets/images/susj.jpg';
 
 function component() {
-var element = document.createElement('div');
+const element = document.createElement('div');
 
 element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 element.classList.add('hello');
+
+const profile = new Image();
+profile.src = Susj;
+profile.classList.add('profile');
+element.appendChild(profile);
 
 return element;
 }
